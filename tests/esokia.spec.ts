8fab87c392d635183d0c01e51c4a7ea5bc296fb3
Ton Nomolivier <on@esokia-webagency.com>
2025-08-19 17:23:58 +0300

    change test e2e

--- tests/esokia.spec.ts
+++
@@ -1,7 +0,0 @@
-// tests/example.spec.ts
-import { test, expect } from '@playwright/test';
-
-test('titre de Google', async ({ page }) => {
-  await page.goto('https://esokia.com/fr');
-  await expect(page).toHaveTitle(/esokia/);
-});
